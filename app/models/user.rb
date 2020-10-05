b6b790cce7b3b2de6e37a064fb29b17761c7fa8e
tamayurabito <tamayurabito@gmail.com>
2020-10-06 08:42:59 +0900

    add validates of devise at models

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_hurigana, :first_name_hurigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+end
