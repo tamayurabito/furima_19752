01aaa5d41a896731bde07ddb495d2401d1ab821d
tamayurabito <tamayurabito@gmail.com>
2020-10-06 16:23:49 +0900

    changed by robocop gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :nickname, :birthday, presence: true
   validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :last_name_hurigana, :first_name_hurigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
 end
