e5c3ae3109584d1bded8fc93397df36d007199ac
tamayurabito <tamayurabito@gmail.com>
2020-10-09 06:29:14 +0900

    fix error of item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  validates :item_name, :item_explanation, :item_status, :delivery_charge_id, :delivery_area_id, :delivery_days_id :price, presence: true
+  validates :item_name, :item_explanation, :item_status, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, presence: true
   belongs_to :user
 end
