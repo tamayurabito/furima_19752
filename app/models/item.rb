873288c003864c7159d34b8b5b4994e83518d3a9
tamayurabito <tamayurabito@gmail.com>
2020-10-12 13:25:26 +0900

    validate & assosiation for ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
   validates :item_name, :item_explanation, :item_status, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name, :item_explanation, :item_category_id, :item_status_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, :image, presence: true
+  validates :item_category_id, :item_status_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, numericality: { other_than: 1 }
   validates :price, inclusion: {in: 300..9999999}
   belongs_to :user
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :delivery_day
 end
