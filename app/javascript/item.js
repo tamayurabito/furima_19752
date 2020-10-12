0cdabef11bc12518057f47afddd78dd69514914a
tamayurabito <tamayurabito@gmail.com>
2020-10-12 16:18:59 +0900

    remove unnecessary code

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,11 +1,8 @@
 $(function() {
   $(".price-input").on("keyup", function() {
     let input = $(".price-input").val();
-    console.log(input);
     let fee = input * 0.1
-    console.log(fee);
     let profit = input - fee
-    console.log(profit);
     $("#add-tax-price").html(fee);
     $("#profit").html(profit);
   });
