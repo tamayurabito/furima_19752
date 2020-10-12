4d2b0ee2e577370203ce7ecd57a7636de4afd3dc
tamayurabito <tamayurabito@gmail.com>
2020-10-12 13:29:34 +0900

    assosiation between item & image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
-  validates :item_name, :item_explanation, :item_status, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, presence: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name, :item_explanation, :item_category_id, :item_status_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, :image, presence: true
   validates :item_category_id, :item_status_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, numericality: { other_than: 1 }
   validates :price, inclusion: {in: 300..9999999}
   belongs_to :user
+  has_one_attached :image
   belongs_to_active_hash :item_category
   belongs_to_active_hash :item_status
   belongs_to_active_hash :delivery_charge
