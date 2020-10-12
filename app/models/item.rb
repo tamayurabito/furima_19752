3fcdb39b9c76be83d5551d89d6aa303c80bdcd80
tamayurabito <tamayurabito@gmail.com>
2020-10-12 13:19:10 +0900

    validate item models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   validates :item_name, :item_explanation, :item_status, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, presence: true
+  validates :item_name, :item_explanation, :item_category_id, :item_status_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, :image, presence: true
+  validates :price, inclusion: {in: 300..9999999}
   belongs_to :user
 end
