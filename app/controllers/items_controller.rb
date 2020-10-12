99e4d0067b0dd762b221250c1accdebc0ecd865a
tamayurabito <tamayurabito@gmail.com>
2020-10-12 13:33:14 +0900

    add create action for items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_explanation, :item_category_id, :item_status_id, 
+      :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
