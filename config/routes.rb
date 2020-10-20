e6b35f9dac8880c85542ea725b66047f42561c6b
tamayurabito <tamayurabito@gmail.com>
2020-10-20 10:36:54 +0900

    Update routes.rb add purchace routes
    purchaceルートを追加。
    itemとの関係からネストにする。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :show]
+  resources :items, only: [:new, :create, :show] do
+    resources :purchace, only: [:index, :create]
+  end
 end
