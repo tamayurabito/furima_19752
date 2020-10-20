27b9a660e763641e91cbbab02e3ce42cb7308eeb
tamayurabito <tamayurabito@gmail.com>
2020-10-20 11:08:17 +0900

    Create purchace_address.rb for FormObject
    Formオブジェクトを実装。
    複数のテーブルにデータを保存する為。

--- app/controllers/purchace_controller.rb
+++ app/controllers/purchace_controller.rb
@@ -1,4 +1,6 @@
 class PurchaceController < ApplicationController
   def index
   end
+    @purchace = PurchaceAddress.new(postal_code: purchace_params[:postal_code], delivery_area_id: purchace_params[:delivery_area_id], citys: purchace_params[:citys], 
+      addresses: purchace_params[:addresses], building_name: purchace_params[:building_name], phone_number: purchace_params[:phone_number], item_id: purchace_params[:item_id], user_id: purchace_params[:user_id])
 end
