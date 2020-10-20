ada8eba8ea77f594522cdba542273237bc71fcf2
tamayurabito <tamayurabito@gmail.com>
2020-10-20 13:08:29 +0900

    add token for validate
    クレジットカードの情報が正しくない場合、tokenが発行されない。
    tokenに対してvalidationでpresence: trueを指定する事でカード情報
    が正しくない場合、tokenが発行されず、tokenが無いのでvalidationで弾かれる。という動作を行う。

--- app/controllers/purchace_controller.rb
+++ app/controllers/purchace_controller.rb
@@ -8,8 +8,7 @@ class PurchaceController < ApplicationController
   end
 
   def create
-    @purchace = PurchaceAddress.new(postal_code: purchace_params[:postal_code], delivery_area_id: purchace_params[:delivery_area_id], citys: purchace_params[:citys],
-                                    addresses: purchace_params[:addresses], building_name: purchace_params[:building_name], phone_number: purchace_params[:phone_number], item_id: purchace_params[:item_id], user_id: purchace_params[:user_id])
+    @purchace = PurchaceAddress.new(purchace_params)
     if @purchace.valid?
       pay_item
       @purchace.save
--- app/forms/purchace_address.rb
+++ app/forms/purchace_address.rb
@@ -1,11 +1,11 @@
 class PurchaceAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number, :purchace_id
+  attr_accessor :item_id, :user_id, :postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number, :purchace_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :delivery_area_id, numericality: { other_than: 1 }
-    validates :citys, :addresses
+    validates :citys, :addresses, :token
     validates :phone_number, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/ }
   end
 
