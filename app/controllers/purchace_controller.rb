a8d2c805dc7c9a9e5221ea4e2bcb0c123b6fc87d
tamayurabito <tamayurabito@gmail.com>
2020-10-20 11:26:49 +0900

    Changed by Rubocop

--- app/controllers/purchace_controller.rb
+++ app/controllers/purchace_controller.rb
@@ -7,29 +7,29 @@ class PurchaceController < ApplicationController
   end
 
   def create
-    @purchace = PurchaceAddress.new(postal_code: purchace_params[:postal_code], delivery_area_id: purchace_params[:delivery_area_id], citys: purchace_params[:citys], 
-      addresses: purchace_params[:addresses], building_name: purchace_params[:building_name], phone_number: purchace_params[:phone_number], item_id: purchace_params[:item_id], user_id: purchace_params[:user_id])
-      if @purchace.valid?
+    @purchace = PurchaceAddress.new(postal_code: purchace_params[:postal_code], delivery_area_id: purchace_params[:delivery_area_id], citys: purchace_params[:citys],
+                                    addresses: purchace_params[:addresses], building_name: purchace_params[:building_name], phone_number: purchace_params[:phone_number], item_id: purchace_params[:item_id], user_id: purchace_params[:user_id])
+    if @purchace.valid?
       pay_item
       @purchace.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
   end
-  
+
   private
-  
+
   def purchace_params
-    params.permit(:token,:postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
+    params.permit(:token, :postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @item[:price],  # 商品の値段
-      card: purchace_params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      amount: @item[:price], # 商品の値段
+      card: purchace_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類(日本円)
     )
   end
 
@@ -44,6 +44,4 @@ class PurchaceController < ApplicationController
   def move_to_top
     redirect_to controller: :items, action: :index if @item.user_id == current_user.id
   end
-
-
 end
--- app/forms/purchace_address.rb
+++ app/forms/purchace_address.rb
@@ -1,10 +1,9 @@
 class PurchaceAddress
-
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id,:postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number, :purchace_id
+  attr_accessor :item_id, :user_id, :postal_code, :delivery_area_id, :citys, :addresses, :building_name, :phone_number, :purchace_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :delivery_area_id, numericality: { other_than: 1 }
     validates :citys, :addresses
     validates :phone_number, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/ }
