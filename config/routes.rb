517dccff5f92bf7942a7724bbb93727a72a74f1a
tamayurabito <tamayurabito@gmail.com>
2020-10-21 11:00:41 +0900

    Update routes.rb to add edit & update
    ルートにeditとupdateを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :show] do
+  resources :items, only: [:new, :create, :show, :edit, :update] do
     resources :purchace, only: [:index, :create]
   end
 end
