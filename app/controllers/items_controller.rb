73ece67f0c6ff8e38e91ec87558a970888e7d4dd
tamayurabito <tamayurabito@gmail.com>
2020-10-21 16:13:07 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_top_another_user, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_top_another_user, only: [:edit, :destroy]
 
