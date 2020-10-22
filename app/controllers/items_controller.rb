4470e1a6a95649f5b3d5d934f85cb3aa19df6dec
tamayurabito <tamayurabito@gmail.com>
2020-10-22 14:39:49 +0900

    Fix edit & destroy action
    購入済商品の詳細画面で編集・削除ボタンを表示しないようにした。
    またURLで直接編集・削除画面に遷移しようとした場合トップページへ遷移する。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_top_another_user, only: [:edit, :destroy]
+  before_action :move_to_top_already_purchased, only: [:edit, :destroy]
 
   def index
     @items = Item.order('id DESC')
@@ -53,4 +54,9 @@ class ItemsController < ApplicationController
   def move_to_top_another_user
     redirect_to root_path unless @item.user_id == current_user.id
   end
+
+  def move_to_top_already_purchased
+    redirect_to root_path if @item.purchace.present?
+  end
+
 end
